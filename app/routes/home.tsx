ff9c05d65a71afa3eecea11ee6bcda16fb484516
ismustel <elias@ustisha.ru>
2025-05-22 20:58:18 +0700

    sidebar is ready without show

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,12 +1,20 @@
 import type { Route } from "./+types/home";
+import React from 'react';
+import  Sidebar from 'app/components.tsx';
+import 'app/app.css'
 
 export function loader() {
-  return { name: "React Router" };
+  return { name: "React Router" ,
+  avatar: 'app/img/avatar.png',
+  countVisits: 1,};
 }
 
 export default function Home({ loaderData }: Route.ComponentProps) {
   return (
     <div className="text-center p-4">
+        <div>
+            <Sidebar AvatarUrl={loaderData.avatar} countVisits={loaderData.countVisits} />
+        </div>
       <h1 className="text-2xl">Hello, {loaderData.name}</h1>
       <a
         className="block mt-2 text-blue-500 underline hover:text-blue-600"
@@ -16,4 +24,6 @@ export default function Home({ loaderData }: Route.ComponentProps) {
       </a>
     </div>
   );
-}
+};
+
+
